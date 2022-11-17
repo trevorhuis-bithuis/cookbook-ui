07b2b75296b3e1d9b306ca7f15fda4864d1b948a
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2022-11-16 23:39:08 -0600

    make project and connect to local app

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+interface Recipe {
+  id: number
+  title: string
+  time_to_complete: number
+}
+
+function App() {
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+
+  const getRecipes = async () => {
+    const response = await fetch('http://localhost:8000/recipes')
+    const data = await response.json()
+    setRecipes(data)
+  }
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+  return (
+    <div className="App">
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.time_to_complete} minutes</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
